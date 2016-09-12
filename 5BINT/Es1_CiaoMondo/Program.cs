04264032d1dc3157c8e5174425f5a5d7234a36d2
Olivelli Putelli <olivelliputelli@live.it>
2016-09-12 14:34:05 +0200

    Es1 Ciao mondo

---
+++ 5BINT/Es1_CiaoMondo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace Es1_CiaoMondo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Nome: ");
+            string nome = ReadLine();
+
+            Console.WriteLine($"Ciao {nome}!!");
+        }
+    }
+}
