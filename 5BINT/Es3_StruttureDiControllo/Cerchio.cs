cd3cdee9f7188b5fcd12671fbbe48e41f2713364
Olivelli Putelli <olivelliputelli@live.it>
2016-09-13 13:55:49 +0200

    Strutture di controllo 5BINT

---
+++ 5BINT/Es3_StruttureDiControllo/Cerchio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es3_StruttureDiControllo
+{
+    class Cerchio
+    {
+        private double raggio = 0.0D;
+
+        public double Raggio
+        {
+            get { return raggio; }
+            set
+            {
+                if (value >= 0) raggio = value;
+                else throw new IndexOutOfRangeException;
+            }
+        }
+
+        public double Altezza { get; set; }
+
+    }
+}
---
+++ 5BINT/Es3_StruttureDiControllo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es3_StruttureDiControllo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c1 = new Cerchio();
+            c1.Raggio = 2;
+            c1.Altezza = 23;
+           
+
+        }
+
+        static double MassimoTraDueNumeri(double n1, double n2)
+        {
+            return (n1 < n2) ? n2 : n1;
+        }
+
+        static double SommaNumeriForeach(double[] numeri)
+        {
+            double somma = 0D;
+            foreach(var numero in numeri)
+                somma += numero;
+            return somma;
+        }
+
+        static double SommaNumeriFor(double[] numeri)
+        {
+            double somma = 0D;
+            for (var i = 0; i< numeri.Length; i++)
+                somma += numeri[i];
+            return somma;
+        }
+
+
+    }
+}
