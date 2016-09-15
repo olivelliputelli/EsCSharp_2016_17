833898e27b90ab2c31d5eabf960d141a2baccb54
Olivelli Putelli <olivelliputelli@live.it>
2016-09-15 11:32:35 +0200

    StreamReader

---
+++ 5BINT/Es4_CicliFor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Console;
+
+namespace Es4_CicliFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Inizio: ");
+            int inizio = Convert.ToInt32(ReadLine());
+            Write("Fine: ");
+            int fine = Convert.ToInt32(ReadLine());
+            Write("Passo: ");
+            int passo = Convert.ToInt32(ReadLine());
+            try
+            {
+                StampaNumeri(inizio, fine, passo);
+            }
+            catch (Exception e)
+            {
+                WriteLine($"Errore!! {e.Message}");
+            }            
+        }
+
+        static void StampaNumeri(int inizio, int fine, int passo)
+        {
+            if(inizio > fine || passo <= 0)
+            {
+                throw new Exception("vsdgdr geg");
+            }
+
+            for (int contatore = inizio; contatore < fine; contatore+=passo)
+            {
+                if (contatore == 7)
+                {
+                    WriteLine("Sette");
+                    break;
+                }
+                WriteLine(contatore);
+            }                        
+        }
+    }
+}
---
+++ 5BINT/Es5_StreamReader/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es5_StreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var numeri = new StreamReader(@"Data\Numeri.txt"))
+            {
+                while (numeri.Peek() >= 0)
+                {
+                    Console.WriteLine(numeri.ReadLine());
+                }
+            }
+        }
+    }
+}
