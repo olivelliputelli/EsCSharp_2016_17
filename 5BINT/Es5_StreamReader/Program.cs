be91490d13be1792ed7785234deead63981e665c
Olivelli Putelli <olivelliputelli@live.it>
2016-09-16 12:15:36 +0200

    Es Switch

--- 5BINT/Es5_StreamReader/Program.cs
+++ 5BINT/Es5_StreamReader/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Console;
 
 namespace Es5_StreamReader
 {
@@ -11,13 +8,51 @@ namespace Es5_StreamReader
     {
         static void Main(string[] args)
         {
-            using (var numeri = new StreamReader(@"Data\Numeri.txt"))
+            try
             {
-                while (numeri.Peek() >= 0)
+                long somma = 0;
+                int c =0, nNumeri =1;
+                long nPrimo, n;
+                using (var sr = new StreamReader(@"Data\Numeri.txt"))
                 {
-                    Console.WriteLine(numeri.ReadLine());
+                    nPrimo = long.Parse((sr.ReadLine()));
+
+                    while (sr.Peek() >= 0)
+                    {
+                        try
+                        {
+                            n= long.Parse((sr.ReadLine()));
+                            somma += n;
+                            nNumeri++;
+                            if(n != nPrimo) c++;                                
+                        }
+                        catch (Exception)
+                        {}                        
+                    }                 
                 }
+                using (StreamWriter sw = new StreamWriter(@"Data\Numeri.txt", true))
+                {
+                    sw.WriteLine();
+                    sw.WriteLine("----------------");
+                    sw.WriteLine($"Somma = {somma}");
+                    double media = (double)somma / (double)nNumeri;
+                    sw.WriteLine($"Media = {media}");
+                    sw.WriteLine($"Numero numeri diversi da {nPrimo} = {c}");
+                }                
             }
+            catch (FileNotFoundException ex)
+            {
+                WriteLine($"{ex.FileName} non esistente!");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                WriteLine("Directory non esistente!");
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+            }
+
         }
     }
 }
---
+++ 5BINT/Es6_Switch/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using static System.Console;
+
+namespace Es6_Switch
+{
+    class Es6
+    {
+        static void Main(string[] args)
+        {
+           
+            for (byte voto = 0; voto < 15; voto++)
+            {
+                WriteLine($"{voto} --> {Giudizio(voto)}");
+            }
+        }
+        /// <summary>
+        /// Returns il giudizio a partire dal voto.
+        /// </summary>
+        /// <param name="voto">Numero che rappresenta il voto.</param>
+        /// <returns>Il giudizio.</returns>
+        static string Giudizio(byte voto)
+        {
+            string giudizio;
+            switch (voto)
+            {
+                case 1:
+                case 2:
+                    giudizio = "Grav. Insuf.!";
+                    break;
+                case 3:
+                case 4:
+                    giudizio = "Insuf.!";
+                    break;
+                case 5:
+                    giudizio = "Quasi Suf.!";
+                    break;
+                case 6:
+                    giudizio = "Suf.!";
+                    break;
+                case 7:
+                case 8:
+                    giudizio = "Buono!";
+                    break;
+                case 9:
+                case 10:
+                    giudizio = "Ottimo!";
+                    break;
+                default:
+                    giudizio = $"{voto} Non è un voto!";
+                    break;
+            }        
+            return giudizio;
+        }
+
+
+
+    }
+}
