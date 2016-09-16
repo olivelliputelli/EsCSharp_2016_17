30fa16d8c3b839bb23275436445161ab289c6187
Olivelli Putelli <olivelliputelli@live.it>
2016-09-16 08:28:46 +0200

    Switch 5AINT

---
+++ 5AINT/Es5_Switch/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using static System.Console;
+
+namespace Es5_Switch
+{
+    class Esercizio5
+    {
+        static void Main(string[] args)
+        {
+            for (int voto = 0; voto < 13; voto++)
+            {
+                WriteLine($"Voto: {voto} ( {Giudizio(voto)} )");
+            }
+        }
+
+        static string Giudizio(int voto)
+        {
+            string giudizio;
+            switch (voto)
+            {
+                case 1:
+                case 2:
+                    giudizio = "Gravemente scarso!";
+                    break;
+                case 3:
+                case 4:
+                    giudizio = "Scarso!";
+                    break;
+                case 5:
+                    giudizio = "Quasi Suff.!!";
+                    break;
+                case 6:
+                    giudizio = "Sufficiente!";
+                    break;
+                case 7:
+                case 8:
+                    giudizio = "Buono!";
+                    break;
+                case 9:
+                case 10:
+                    giudizio = "Ottimo!";
+                    break;
+                default:
+                    giudizio = "NON voto valido!!";
+                    break;
+            }
+            return giudizio;
+        }
+
+
+    }
+}
