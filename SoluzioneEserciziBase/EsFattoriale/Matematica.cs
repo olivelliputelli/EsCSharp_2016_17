7b897307ea276b7ea99c65dad4c52e44f8fa7cc3
Olivelli Putelli <olivelliputelli@live.it>
2016-09-20 13:47:55 +0200

    Es Fattoriale

---
+++ SoluzioneEserciziBase/EsFattoriale/Matematica.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EsFattoriale
+{
+    public static class Matematica
+    {
+        public static ulong Fattoriale(byte n)
+        {
+            if (n > 64) //ulong non riesce a contenere 65!
+                throw new ArgumentOutOfRangeException();
+
+            ulong fat = 1;
+            for (ulong i = 1; i <= (ulong)n; i++)
+            {
+                fat *= i;
+            }
+            return fat;
+        }
+        /// <summary>
+        /// Fattoriale ricorsivo
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static ulong FattorialeRic(ulong n)
+        {
+            if (n > 64)
+                throw new ArgumentOutOfRangeException();
+
+            if (n >= 1)
+                return n * FattorialeRic(n - 1);
+            else
+                return 1;
+        }
+    }
+}
---
+++ SoluzioneEserciziBase/EsFattoriale/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsFattoriale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("N: ");
+            //byte n = byte.Parse(Console.ReadLine());
+            for (byte i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(i + " --> " + Matematica.Fattoriale(i) + " ---> " + Matematica.FattorialeRic(i));
+            }
+
+        }
+    }
+}
