1c0cb827839be7506a57458e0e501f19e90316ac
Olivelli Putelli <olivelliputelli@live.it>
2016-09-20 12:42:13 +0200

    esercizio giorni mese

--- SoluzioneEserciziBase/EsNumeroGiorniMese/Program.cs
+++ SoluzioneEserciziBase/EsNumeroGiorniMese/Program.cs
@@ -8,26 +8,92 @@ namespace EsNumeroGiorniMese
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Lista anni bisestili");
-            for (int anno = 1900; anno <= 3000; anno++)
+            /* Codice per testare la funzione IsBisestile */
+            //Console.WriteLine("Lista anni bisestili");
+            //for (int anno = 1900; anno <= 3000; anno++)
+            //{
+            //    if (IsBisestile(anno)) // IsBisestile(anno) == true
+            //        Console.WriteLine(anno);
+            //}
+
+            Mese mese;
+            int numeroGiorni = 0;
+            Console.Write("Mese (in lettere): ");
+
+            /* Metodo con Parse() */
+            //try
+            //{
+            //    mese = (Mese)Enum.Parse(typeof(Mese), Console.ReadLine());
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine($"Input non valido!");
+            //}
+
+            /* Metodo con TryParse<>() */
+            if (!Enum.TryParse<Mese>(Console.ReadLine(), out mese))
             {
-                if (IsBisestile(anno)) // IsBisestile(anno) == true
-                    Console.WriteLine(anno);
+                Console.WriteLine($"Input non valido!");
             }
+            else
+            {                
+                switch (mese)
+                {
+                    case Mese.Gennaio:
+                    case Mese.Marzo:
+                    case Mese.Maggio:
+                    case Mese.Luglio:
+                    case Mese.Agosto:
+                    case Mese.Ottobre:
+                    case Mese.Dicembre:
+                        numeroGiorni = 31;
+                        break;
+                    case Mese.Febbraio:
+                        Console.Write("Anno: ");
+                        int anno = int.Parse(Console.ReadLine());
+                        if (IsBisestile(anno))
+                            numeroGiorni = 29;
+                        else
+                            numeroGiorni = 28;
+                        break;
+                    case Mese.Aprile:
+                    case Mese.Giugno:
+                    case Mese.Settembre:
+                    case Mese.Novembre:
+                        numeroGiorni = 30;
+                        break;
+                }
+                Console.WriteLine("Numero giorni = {0}", numeroGiorni);
+            }           
         }
 
         static bool IsBisestile(int anno)
         {
             if (!(anno >= 1900 && anno <= 3000))
                 throw new ArgumentOutOfRangeException();
-            
-            if((anno % 4 == 0) && (anno % 100 != 0 || anno % 400 == 0))
+
+            if ((anno % 4 == 0) && (anno % 100 != 0 || anno % 400 == 0))
             {
                 return true;
             }
             return false;
         }
+    }
+    enum Mese
+    {
+        Gennaio = 1,
+        Febbraio,
+        Marzo,
+        Aprile,
+        Maggio,
+        Giugno,
+        Luglio,
+        Agosto,
+        Settembre,
+        Ottobre,
+        Novembre,
+        Dicembre
     }
 }
