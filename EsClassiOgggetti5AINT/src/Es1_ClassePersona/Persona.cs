a9b75be1885887755eb34b7bf7824f755785b031
Olivelli Putelli <olivelliputelli@live.it>
2016-09-26 11:27:58 +0200

    EsClassePersona

---
+++ EsClassiOgggetti5AINT/src/Es1_ClassePersona/Persona.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Es1_ClassePersona
+{
+    public class Persona
+    {
+        public int PersonaId { get; set; }
+        [Required(ErrorMessage = "Il {0} lo devi mettere!")]
+        [StringLength(20, ErrorMessage ="{0} massimo {1} caratteri!")]
+        public string Cognome { get; set; }
+        [MinLength(3, ErrorMessage = "Minima lunghezza del {0} e' {1}!")]
+        public string Nome { get; set; }
+
+        [Range(1900, 2050, ErrorMessage = "L'anno della data di nascita deve essere tra {1} e {2}!")]
+        public int AnnoDiNascita { get; set; }
+
+        [RegularExpression(@"[1-5][A-F]", ErrorMessage ="La sigla {0} non e' valida!")]
+        public string ClasseFrequentata { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Cognome} {this.Nome} del {this.AnnoDiNascita} frequenta la {this.ClasseFrequentata} ";
+        }
+    }
+}
---
+++ EsClassiOgggetti5AINT/src/Es1_ClassePersona/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Es1_ClassePersona
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Persona p1 = new Persona();
+            Console.Write("Cognome: ");
+            p1.Cognome = Console.ReadLine();
+            Console.Write("Nome: ");
+            p1.Nome = Console.ReadLine();
+            Console.Write("Anno di nascita: ");
+            p1.AnnoDiNascita = int.Parse(Console.ReadLine());
+            Console.Write("Classe frequentata: ");
+            p1.ClasseFrequentata = Console.ReadLine();
+
+            var listaProblemi = new List<ValidationResult>();
+            var vc = new ValidationContext(p1);
+            var isValid = Validator.TryValidateObject(p1, vc, listaProblemi, true);
+            if (isValid)
+            {
+                Console.WriteLine("Tutto OK");
+            }
+            else
+                foreach (var item in listaProblemi)
+                {
+                    Console.WriteLine(item.ErrorMessage);
+                }
+        }
+    }
+}
