226eb87ffa1dc97e222905f1f331d46697419f72
Olivelli Putelli <olivelliputelli@live.it>
2016-11-07 11:18:53 +0100

    Northwind

---
+++ Northwind/NorthwindDB/Corrieri.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE Corrieri
+(
+   CorriereId CHAR(3) NOT NULL,
+   Denominazione NVARCHAR(50) NOT NULL,
+   Telefono CHAR(20) NULL,
+   TelefonoCellulare CHAR(20) NULL,
+   IndirizzoEMail VARCHAR(50) NULL,
+   DataUltimaModifica DATETIME DEFAULT(GETDATE()) NOT NULL,
+   CONSTRAINT PK_Corrieri PRIMARY KEY(CorriereId), 
+   CONSTRAINT CK_IndirizzoEMail CHECK (IndirizzoEMail LIKE '%@%')
+)
---
+++ Northwind/NorthwindDB/Fornitori.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE Fornitori
+(
+	[FornitoreId] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [NomeFornitore] NVARCHAR(50) NOT NULL, 
+    [NomeContatto] NVARCHAR(30) NULL, 
+    [TitoloContatto] NVARCHAR(30) NULL, 
+    [Indirizzo] NVARCHAR(60) NULL, 
+    [Città] NVARCHAR(30) NULL, 
+    [CodicePostale] CHAR(5) NULL, 
+    NazioneId CHAR(2) NULL DEFAULT ('IT'), 
+    [Telefono] CHAR(20) NULL, 
+    [Fax] CHAR(20) NULL,
+	HomePage CHAR(50),
+	CONSTRAINT FK_Nazioni_Fornitori FOREIGN KEY (NazioneId) REFERENCES Nazioni(NazioneId) 
+)
